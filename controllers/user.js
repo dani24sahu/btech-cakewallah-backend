eb5a8dabff8c3fb37e8d958f9aa5b1be3bbad1d0
Dani <daniramsahu24@gmail.com>
2023-05-19 10:54:32 +0530

    Added Nodemailer package for contactUs API

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,8 @@
 import { asyncError } from "../middlewares/errorMiddleware.js";
 import { User } from "../models/User.js";
 import { Order } from "../models/Order.js";
+import ErrorHandler from "../utils/ErrorHandler.js";
+import { sendEmail } from "../utils/sendEmail.js";
 
 export const myProfile = (req, res, next) => {
   res.status(200).json({
@@ -29,6 +31,24 @@ export const getAdminUsers = asyncError(async (req, res, next) => {
   res.status(200).json({
     success: true,
     users,
+  });
+});
+
+export const contact = asyncError(async (req, res, next) => {
+  const { name, email, message } = req.body;
+
+  if (!name || !email || !message)
+    return next(new ErrorHandler("All fields are mandatory", 400));
+
+  const to = process.env.MY_MAIL;
+  const subject = "Contact from BTech Cake Wallah";
+  const text = `I am ${name} and my Email is ${email}.\n${message}`;
+
+  await sendEmail(to, subject, text);
+
+  res.status(200).json({
+    success: true,
+    message: "Your message has been sent.",
   });
 });
 
--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -11,3 +11,5 @@ export const errorMiddleware = (err, req, res, next) => {
 export const asyncError = (passedFunction) => (req, res, next) => {
   Promise.resolve(passedFunction(req, res, next)).catch(next);
 };
+
+
